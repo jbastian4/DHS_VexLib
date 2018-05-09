12848ccdb2721137b7fb8082e01c4abb51c431bd
jbastian4 <jbastian4@gmail.com>
2018-05-09 11:01:53 -0600

    Worked on SU

--- superuser.c
+++ superuser.c
@@ -40,3 +40,24 @@ void initializePIDGroup(pidGroup *group, float kp, float ki, float kd)
   group->ki = kd;
   group->kd = kd;
 }
+
+void setMotors(motorGroup *group, byte power)
+{
+  for (int i=0; i<group->numMotors; i++) //set motors
+		motor[group->motors[i]] = power;
+}
+
+byte pid(pidGroup *pid, int request)
+{
+  gyroCurrentValue = SensorValue[gyroPort];
+
+  pid->err = pid->RequestedValue - pid->currentValue;
+  pid->intg = pid->intg + gyroErr;
+  pid->der = pid->err - pid->prevErr;
+  pid->dt = nPgmTime - pid->prevTime;
+
+  return ((pid->kp * pid->err) + (pid->ki * pid->intg * pid->dt) + (pid->kd * pid->der / pid->dt));
+
+  pid->prevErr = pid->err;
+  pid->prevTime = nPgmTime;
+}
